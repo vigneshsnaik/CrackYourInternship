78d204fa4c655b5e55b867e8cbb7015f98e0125e
VIGNESH S NAIK <vigneshsnaik03@gmail.com>
2024-07-17 22:52:47 +0530

    Create best-time-to-buy-and-sell-stock.cpp

---
+++ best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min = prices[0];
+        int max_profit = 0;
+        for (int i = 1; i < prices.size(); ++i) {
+            if (prices[i] < min)
+                min = prices[i];
+            int profit = prices[i] - min;
+            if (profit > max_profit)
+                max_profit = profit;
+        }
+        return max_profit;
+    }
+};
