19e411bc3d1e017ac7f53bad9f2bb57c986c1486
VIGNESH S NAIK <vigneshsnaik03@gmail.com>
2024-07-17 22:57:27 +0530

    Create ontainer-with-most-water.cpp

---
+++ ontainer-with-most-water.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        auto l = height.begin();
+        auto r = height.end() - 1;
+        int maxArea = 0;
+        while (l < r) {
+            int area = (*l < *r ? *l : *r) * (r - l);
+            maxArea = (area > maxArea ? area : maxArea);
+            if (*l < *r) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return maxArea;
+    }
+};
