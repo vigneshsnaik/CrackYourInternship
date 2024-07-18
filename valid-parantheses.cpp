1eabdcefaaace157e9c6b9eaaaa781354f70f4b5
VIGNESH S NAIK <vigneshsnaik03@gmail.com>
2024-07-18 22:43:02 +0530

    Create valid-parantheses.cpp

---
+++ valid-parantheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> t;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == ')') {
+                if (t.empty() || t.top() != '(') {
+                    return false;
+                }
+                t.pop();
+            } else if (s[i] == ']') {
+                if (t.empty() || t.top() != '[') {
+                    return false;
+                }
+                t.pop();
+            } else if (s[i] == '}') {
+                if (t.empty() || t.top() != '{') {
+                    return false;
+                }
+                t.pop();
+            } else {
+                t.push(s[i]);
+            }
+        }
+        return t.empty();
+    }
+};
