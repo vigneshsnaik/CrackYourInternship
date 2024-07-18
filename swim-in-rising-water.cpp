d93c4cc56305e7e58e37b01822524306dac08516
VIGNESH S NAIK <vigneshsnaik03@gmail.com>
2024-07-18 22:56:58 +0530

    Create swim-in-rising-water.cpp

---
+++ swim-in-rising-water.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<vector<int>>> dp;
+    vector<vector<bool>> visited;
+    int n;
+    int solve(vector<vector<int>>& grid, int x, int y, int time) {
+        if (x < 0 or y < 0 or x == n or y == n or visited[x][y])
+            return INT_MAX;
+        if (x == n - 1 and y == n - 1)
+            return max(time, grid[x][y]);
+        if (dp[x][y][time] != -1)
+            return dp[x][y][time];
+        int newTime = max(time, grid[x][y]);
+        visited[x][y] = true;
+        dp[x][y][time] = min({
+            solve(grid, x - 1, y, newTime),
+            solve(grid, x + 1, y, newTime),
+            solve(grid, x, y + 1, newTime),
+            solve(grid, x, y - 1, newTime),
+        });
+        visited[x][y] = false;
+        return dp[x][y][time];
+    }
+    int swimInWater(vector<vector<int>>& grid) {
+        n = grid.size();
+        dp.resize(n, vector<vector<int>>(n, vector<int>(n * n, -1)));
+        visited.resize(n, vector<bool>(n, false));
+        return solve(grid, 0, 0, 0);
+    }
+};
