c1ebfe5e03afd7c95aa8976aa85c3beb1819060d
VIGNESH S NAIK <vigneshsnaik03@gmail.com>
2024-07-21 23:20:16 +0530

    Create remove-k-digits.cpp

---
+++ remove-k-digits.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        if (k == num.length())
+            return "0";
+        int i = 0;
+        while (i < num.length() - 1 && k) {
+            if (num[i + 1] < num[i]) {
+                num.erase(num.begin() + i);
+                k--;
+                if (i) {
+                    i--;
+                }
+            } else {
+                i++;
+            }
+        }
+        while (k) {
+            num.erase(num.begin() + (i--));
+            k--;
+        }
+        while (num[0] == '0') {
+            num.erase(num.begin());
+        }
+        return num.length() ? num : "0";
+    }
+};
