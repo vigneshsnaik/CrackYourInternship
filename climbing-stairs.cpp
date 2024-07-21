980b18d49f8b222a800a7fc0481761b925edc225
VIGNESH S NAIK <vigneshsnaik03@gmail.com>
2024-07-21 23:34:28 +0530

    Create climbing-stairs.cpp

---
+++ climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> val = {1, 2};
+    int helper(int n) {
+        if (val[n - 1])
+            return val[n - 1];
+        val[n - 1] = helper(n - 1) + helper(n - 2);
+        return val[n - 1];
+    }
+    int climbStairs(int n) {
+        val.resize(n, 0);
+        return helper(n);
+    }
+};
