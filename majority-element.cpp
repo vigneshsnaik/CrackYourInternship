b0b37c60733250e8781c282e79b5af0b048b6072
VIGNESH S NAIK <vigneshsnaik03@gmail.com>
2024-07-21 23:31:22 +0530

    Create majority-element.cpp

---
+++ majority-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int maj = 0, count = 0;
+        for (int num : nums)
+            if (num == maj) {
+                count++;
+            } else if (count == 0) {
+                maj = num;
+            } else {
+                count--;
+            }
+        return maj;
+    }
+};
