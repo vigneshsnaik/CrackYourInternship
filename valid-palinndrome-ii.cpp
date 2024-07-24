922c7812d777a0cd058d283c33b03364ba6d004e
VIGNESH S NAIK <vigneshsnaik03@gmail.com>
2024-07-24 14:02:25 +0530

    Create valid-palinndrome-ii.cpp

---
+++ valid-palinndrome-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(const string& s, int l, int r) {
+        while (l < r)
+            if (s[l++] != s[r--])
+                return false;
+        return true;
+    }
+    bool validPalindrome(const string& s) {
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (s[l] != s[r])
+                return isPalindrome(s, l + 1, r) or isPalindrome(s, l, r - 1);
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
