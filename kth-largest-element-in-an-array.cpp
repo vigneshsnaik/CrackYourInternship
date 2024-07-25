6107481f0068e5eeeeb996a2ce3fbdef3afc6c94
VIGNESH S NAIK <vigneshsnaik03@gmail.com>
2024-07-25 19:45:20 +0530

    Create kth-largest-element-in-an-array.cpp

---
+++ kth-largest-element-in-an-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int>q;
+        for(int num:nums)
+            q.push(num);
+        while(--k)
+            q.pop();
+        return q.top();
+    }
+};
