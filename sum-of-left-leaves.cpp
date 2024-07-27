4d952688080acf0cdcdb9223f4dda524d2834b4b
VIGNESH S NAIK <vigneshsnaik03@gmail.com>
2024-07-27 23:47:48 +0530

    Create sum-of-left-leaves.cpp

---
+++ sum-of-left-leaves.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if (root)
+            if (root->left)
+                return (!root->left->left and !root->left->right
+                            ? root->left->val
+                            : 0) +
+                       sumOfLeftLeaves(root->left) +
+                       sumOfLeftLeaves(root->right);
+            else
+                return sumOfLeftLeaves(root->left) +
+                       sumOfLeftLeaves(root->right);
+        return 0;
+    }
+};
